9d1076ebd7996d1896f3e09c244fb5fabc2c8bc5
Muhammad Yovie Ridwan <110608482+yovieridwan17@users.noreply.github.com>
2026-02-27 22:35:21 +0700

    Update build.gradle.kts

--- english_learning_app/android/build.gradle.kts
+++ english_learning_app/android/build.gradle.kts
@@ -13,8 +13,8 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
+    proect.layout.buildDirectory.value(newSubprojectBuildDir)
+
 subprojects {
     project.evaluationDependsOn(":ap.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
